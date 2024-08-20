6cca86b3b348ee889896cd539e6f93b1b11a274b
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-08-20 02:43:25 +0100

    Webpack - Task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./js/dashboard_main.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        use: [
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  performance: {
+    hints: "warning",
+    maxAssetSize: 200000, // Set the size limit for assets to 200KB
+  },
+};
