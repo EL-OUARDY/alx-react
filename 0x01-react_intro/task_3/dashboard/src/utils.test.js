03d4b9841a57be22fc3098443ed678420036d4ed
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-08-21 14:03:04 +0100

    React intro - Task 6 - install enzyme

--- 0x01-react_intro/task_3/dashboard/src/utils.test.js
+++ 0x01-react_intro/task_3/dashboard/src/utils.test.js
@@ -3,19 +3,16 @@ import { getLatestNotification, getFullYear, getFooterCopy } from "./utils";
 describe("Test Utils.js", () => {
   it("getFullYear function", (done) => {
     expect(getFullYear()).toBe(new Date().getFullYear());
-    done();
   });
 
   it("getFooterCopy function", (done) => {
     expect(getFooterCopy(true)).toBe("Holberton School");
     expect(getFooterCopy(false)).toBe("Holberton School main dashboard");
-    done();
   });
 
   it("getLatestNotification function", (done) => {
     expect(getLatestNotification()).toBe(
       "<strong>Urgent requirement</strong> - complete by EOD"
     );
-    done();
   });
 });
