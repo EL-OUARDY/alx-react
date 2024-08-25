5f99821fb38bbbb555797a56843da84678e50813
EL-OUARDY <ouadie.elouardy@gmail.com>
2024-08-25 14:09:06 +0100

    React props - FIX: write tests for task 1

--- 0x02-react_props/task_1/dashboard/src/App/App.test.js
+++ 0x02-react_props/task_1/dashboard/src/App/App.test.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Login from "../Login/Login";
+import Notifications from "../Notifications/Notifications";
 
 describe("Test App.js", () => {
   it("App without crashing", () => {
@@ -8,18 +12,19 @@ describe("Test App.js", () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  it("contains a div with the class App-header", () => {
+  it("should render Notifications, Header, Login, and Footer components", () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(".App-header").exists()).toBe(true);
-  });
 
-  it("contains a div with the class App-body", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(".App-body").exists()).toBe(true);
-  });
+    // Verify that the Notifications component is rendered
+    expect(wrapper.find(Notifications).length).toBe(1);
 
-  it("contains a div with the class App-footer", () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(".App-footer").exists()).toBe(true);
+    // Verify that the Header component is rendered
+    expect(wrapper.find(Header).length).toBe(1);
+
+    // Verify that the Login component is rendered
+    expect(wrapper.find(Login).length).toBe(1);
+
+    // Verify that the Footer component is rendered
+    expect(wrapper.find(Footer).length).toBe(1);
   });
 });
